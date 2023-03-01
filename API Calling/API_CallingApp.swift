7184f4a6030a24ba979e0eb636f873751dcde652
Daksh <dnakra@bsd220.org>
2023-03-01 09:27:02 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Daksh Nakra on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
